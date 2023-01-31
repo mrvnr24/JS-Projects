158bbda0f59a9ad78b8b802d6f7a73592475e991
Mervenur Aytekin <mrvnre24@gmail.com>
2023-01-31 11:08:00 -0600

    first commit

---
+++ 01-31-2023/js/index1.js
@@ -0,0 +1,15 @@
+function test1() {
+    alert("dosya yolundan baglanti kuruldu");
+}
+
+function bir() {
+    document.getElementById("x").src="img/tree.jfif";
+}
+
+function iki() {
+    document.getElementById("x").src="img/tree2.jfif";
+}
+
+function uc() {
+    document.getElementById("x").src="img/tree3.jfif";
+}
