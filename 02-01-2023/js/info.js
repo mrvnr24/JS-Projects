cfc1c634d8c95bc650b0eb1947205b77e523a259
Mervenur Aytekin <mrvnre24@gmail.com>
2023-02-01 11:01:00 -0600

    js, css, html, bootstrap

---
+++ 02-01-2023/js/info.js
@@ -0,0 +1,10 @@
+/*let a=24;
+document.getElementById("xx").innerHTML = typeof a;
+*/
+
+/*var b = 63;
+b = 45;
+document.getElementById("xx").innerHTML = b;*/
+
+const c = 78;
+document.getElementById("xx").innerHTML = c;
