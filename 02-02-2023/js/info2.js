753ce8d834ff6628ef12d6b2766c67a452539062
Mervenur Aytekin <mrvnre24@gmail.com>
2023-02-02 11:10:10 -0600

    bootstrap, html, js

---
+++ 02-02-2023/js/info2.js
@@ -0,0 +1,8 @@
+function Renk() {
+    document.getElementById("aa").style.color = "red";
+}
+
+function Welcome() {
+    var x = document.getElementById("ii").value;
+    document.getElementById("aa").innerHTML = "Merhaba " + x + ". Web Siteme Hosgeldiniz!";
+}
