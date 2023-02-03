aa9573f7d8f86d1b377f4984a4ae594624d2303b
Mervenur Aytekin <mrvnre24@gmail.com>
2023-02-03 10:47:35 -0600

    calculation function

---
+++ 02-03-2023/js/info.js
@@ -0,0 +1,31 @@
+function Kontrol() {
+    //alert("bu alana yapistirmayin.");
+    document.getElementById("hh").innerHTML = "bu alana yapistirmayin.";
+    document.getElementById("hh").style.color = "blue";
+}
+
+function Mouseover() {
+    document.getElementById("pp").style.color = "red";
+    document.getElementById("pp").style.fontSize = "100px";
+    document.getElementById("pp").innerHTML = "Hello!"
+}
+
+function MouseOut() {
+    document.getElementById("pp").style.color = "blueviolet";
+    document.getElementById("pp").style.fontSize = "25px";
+    document.getElementById("pp").innerHTML = "Merhaba!";
+
+}
+
+function Kopyala() {
+    alert("Lutfen bilgileri kopyalamayin");
+}
+
+function CutControl() {
+    alert("Kesme islemi yaptiniz");
+}
+
+function SelectControl() {
+    alert("test");
+}
+
---
+++ 02-03-2023/js/info2.js
@@ -0,0 +1,5 @@
+function Topla() {
+    var x = document.getElementById("in1").value;
+    var y = document.getElementById("in2").value;
+    document.getElementById("h5").innerHTML = Number(x) + Number(y);
+}
